6cca4bf90baf5708a9df2bb842d7aeaac3a4aa01
MaxFrolow <maxfrolow77@gmail.com>
2022-06-16 14:45:07 +0300

    added basic routes

--- front/mywiki/src/App.js
+++ front/mywiki/src/App.js
@@ -1,24 +1,20 @@
 import * as React from "react";
-import Todo from "./components/Todo"
+import Todo from "./components/Todo";
 import { Routes, Route, Link } from "react-router-dom";
-
-
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoriteMeetupsPage from "./pages/Favorites";
 
 function App() {
   return (
     <div className="App">
-
-
-
-      <h1>My Todods</h1>
-      <Todo title='Learn something'/>
-      <Todo title='Buy something'/>
-      <Todo title='Clean up'/>
-      
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/new-meetup" element={<NewMeetupPage />} />
+        <Route path="/favorite" element={<FavoriteMeetupsPage />} />
+      </Routes>
     </div>
   );
-};
+}
 
-
-
-export default App
+export default App;
